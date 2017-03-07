744dacd71c4a46104b9ddad8b7b80f0105f52c07
Quadrifoglio <clement@dreamvids.fr>
2017-03-07 22:27:07 +0100

    kvm: added parameters: acceleration, cpus, memory, vnc

--- scripts/kvm/vm/start.py
+++ scripts/kvm/vm/start.py
@@ -4,16 +4,39 @@ import sys
 import time
 import json
 import subprocess
-import os
 
 vm = json.loads(sys.argv[1])
+params = vm['parameters']
+
 folder = '/var/lib/olvm/vm/' + vm['name']
 disk = folder + '/disk.qcow2'
 
-child = subprocess.Popen(['/usr/bin/qemu-system-x86_64', '-nographic'],
-    stdout = subprocess.PIPE,
-    stderr = subprocess.PIPE
-)
+opts = [
+    '/usr/bin/qemu-system-x86_64',
+    '-nographic',
+    '-drive', 'format=qcow2,file=' + disk
+]
+
+if 'acceleration' in params:
+    opts.append('-enable-kvm')
+
+if 'cpus' in params:
+    opts.append('-smp')
+    opts.append(params['cpus'])
+
+if 'memory' in params:
+    opts.append('-m')
+    opts.append(params['memory'])
+
+if 'vnc' in params:
+    opts.append('-vnc')
+
+    if 'vncws' in params:
+        opts.append(params['vnc'] + ',websocket=' + params['vncws'])
+    else:
+        opts.append(params['vnc'])
+
+child = subprocess.Popen(opts, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
 time.sleep(2)
 child.poll()
